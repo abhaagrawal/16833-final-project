9132febddfa3e9d34822659735e3e0182842cd5d
Calen Robinson <lokera6@gmail.com>
2020-11-10 15:04:44 -0500

    feat(csv_scale.py): add csv_scale.py
    Adds a python file to scale every entry in a csv file by a constant value

---
+++ csv_scale.py
@@ -0,0 +1,25 @@
+import argparse as ap
+import csv
+
+parser = ap.ArgumentParser(description = "Multiply every value in a .csv by a constant")
+parser.add_argument('infile',action='store',type=str)
+parser.add_argument('scale',action='store',type=int)
+parser.add_argument('-o',action='store',type=str,required=False,default='out.csv')
+parser.add_argument('--no_header',action='store_false')
+args = parser.parse_args();
+
+with open(args.infile, newline='') as infile:
+	reader = csv.reader(infile)
+	with open(args.o, 'w+', newline='',) as outfile:
+		writer = csv.writer(outfile)
+
+		# Skip header
+		if args.no_header:
+			writer.writerow(next(reader))
+			print("Skipped header")
+
+		for line in reader:
+			for i in range(0,len(line)):
+				line[i] = str(float(line[i]) * args.scale)
+			writer.writerow(line)
+
