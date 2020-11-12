7bbd4d5981f3c284be5efd972024bd4f9a6ac6a2
Calen Robinson <lokera6@gmail.com>
2020-11-11 20:44:14 -0500

    feat(vis_vo.m): add feature to autosave figures
    Automatically saves .fig and .png files from various viewpoints

--- vis_vo.m
+++ vis_vo.m
@@ -12,8 +12,7 @@ system(strcat("python csv_scale.py ",coord_path,...
 
 vo = readmatrix(scaled_coord_doc);
 
-
-%%
+%% Calculate
 pos = zeros(4,size(vo,1)+1);
 rot = eye(3);
 for i = 1:size(vo,1)
@@ -23,20 +22,83 @@ for i = 1:size(vo,1)
     rot = rotz(rad2deg(vo(i,6)/scale))...
         * roty(rad2deg(vo(i,5)/scale))...
         * rotx(rad2deg(vo(i,4)/scale)) * rot;
-    
-    %plot3(pos(1,1:i),pos(2,1:i),pos(3,1:i))
-    %grid on
-    %xlabel("X")
-    %ylabel("Y")
-    %zlabel("Z")
-    %pause(0.001)
 end
+pos = pos / scale;
 
-plot3(pos(1,:),pos(2,:),pos(3,:))
+%% Plot and save figures
+close all
+fig_3d = figure;
+plot3(pos(1,:),pos(2,:),pos(3,:));
+hold on;
+plot3(pos(1,1),pos(2,1),pos(3,1),'go');
+plot3(pos(1,end),pos(2,end),pos(3,end),'ro');
+legend("Path","Start","Finish")
+hold off
+xlabel("X (m)")
+ylabel("Y (m)")
+zlabel("Z (m)")
 grid on
-xlabel("X")
-ylabel("Y")
-zlabel("Z")
+title("3D view of path from visual odometry ")
+
+fig_xy = figure;
+plot(pos(1,:),pos(2,:))
+hold on
+plot(pos(1,1),pos(2,1),'go')
+plot(pos(1,end),pos(2,end),'ro')
+hold off;
+legend("Path","Start","Finish")
+legend('Location','southwest')
+xlabel("X (m)")
+ylabel("Y (m)")
+title("Car Path from VO. Top View")
+
+fig_xz = figure;
+plot(pos(1,:),pos(3,:))
+hold on
+plot(pos(1,1),pos(3,1),'go')
+plot(pos(1,end),pos(3,end),'ro')
+hold off
+legend("Path","Start","Finish")
+legend('Location','northwest')
+xlabel("X (m)")
+ylabel("Z (m)")
+title("Car Path from VO. Side view (looking in +y direction)")
+
+fig_yz = figure;
+plot(pos(2,:),pos(3,:))
+hold on
+plot(pos(2,1),pos(3,1),'go')
+plot(pos(2,end),pos(3,end),'ro')
+hold off
+legend("Path","Start","Finish")
+xlabel("Y (m)")
+ylabel("Z (m)")
+title("Car Path from VO. Side view (looking in -x direction)")
+
+image_folder = 'images';
+mkdir(image_folder);
+saveas(fig_3d,strcat(image_folder,'/vo_3d'),'fig')
+saveas(fig_3d,strcat(image_folder,'/vo_3d'),'png')
+saveas(fig_xy,strcat(image_folder,'/vo_xy'),'fig')
+saveas(fig_xy,strcat(image_folder,'/vo_xy'),'png')
+saveas(fig_xz,strcat(image_folder,'/vo_xz'),'fig')
+saveas(fig_xz,strcat(image_folder,'/vo_xz'),'png')
+saveas(fig_yz,strcat(image_folder,'/vo_yz'),'fig')
+saveas(fig_yz,strcat(image_folder,'/vo_yz'),'png')
+close all
+%% Plot fancy
+
+max_dim = max(pos(1:3,:),[],'all');
+min_dim = min(pos(1:3,:),[],'all');
+for i = 1:size(pos,2)
+    plot3(pos(1,1:i),pos(2,1:i),pos(3,1:i))
+    grid on
+    
+    %xlim([min_dim max_dim])
+    %ylim([min_dim max_dim])
+    %zlim([min_dim max_dim])
+    pause(0.00001)
+end
 % Limit axis
 %ylim(xlim-(sum(xlim)/2));
 %xlim(xlim-(sum(xlim)/2));
