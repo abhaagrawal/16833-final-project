edafe11f969308c0797402b0c85ea49424229e03
Calen Robinson <lokera6@gmail.com>
2020-12-06 23:49:36 -0500

    feat(data): add zero origin and scaled csv files

--- Data_Acquisition/get_vo.m
+++ Data_Acquisition/get_vo.m
@@ -14,29 +14,32 @@ else
 end
 coord_doc = "vo-coord.csv";
 time_doc = "vo-time.csv";
-scaled_coord_doc = "Data/vo-coord-scaled.csv";
-zero_origin_time_doc = "Data/vo-time-zero-origin.csv";
-zero_origin_time_scaled_doc = "Data/vo-time-zero-origin-scaled.csv";
+scaled_coord_doc = "vo-coord-scaled.csv";
+zero_origin_time_doc = "vo-time-zero-origin.csv";
+zero_origin_time_scaled_doc = "vo-time-zero-origin-scaled.csv";
 vo_scale = 10000;
 
 coord_path = strcat(data_dir,coord_doc);
 time_path = strcat(data_dir,time_doc);
+scaled_coord_path = strcat(data_dir,scaled_coord_doc);
+zero_origin_time_path = strcat(data_dir,zero_origin_time_doc);
+zero_origin_time_scaled_path = strcat(data_dir,zero_origin_time_scaled_doc);
 % Call python script to scale the data
 system(strcat("python Pre_Processing/csv_scale.py ",coord_path,...
-    " ",string(vo_scale)," -o ",scaled_coord_doc));
+    " ",string(vo_scale)," -o ",scaled_coord_path));
 
 time_scale = 1000000;
 % Call python script to zero origin the data
-system(strcat("python Pre_Processing/csv_zero_origin.py ",time_path,...
-    " -o ",zero_origin_time_doc));
+assert(system(strcat("python Pre_Processing/csv_zero_origin.py ",time_path,...
+    " -o ",zero_origin_time_path)) == 0);
 
 % Call python script to scale origin the data
-system(strcat("python Pre_Processing/csv_scale.py ",zero_origin_time_doc,...
-    " ",string(time_scale)," -o ",zero_origin_time_scaled_doc,...
-    " --div "));
+assert(system(strcat("python Pre_Processing/csv_scale.py ",zero_origin_time_path,...
+    " ",string(time_scale)," -o ",zero_origin_time_scaled_path,...
+    " --div ")) == 0);
 % time = readmatrix(zero_origin_time_scaled_doc);
 
-vo = readmatrix(scaled_coord_doc);
+vo = readmatrix(scaled_coord_path);
 time = readmatrix(time_path);
 
 end
