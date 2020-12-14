9ee02c466d3416f10565dfc43e50d9d06166e2d7
Calen Robinson <lokera6@gmail.com>
2020-12-13 20:04:06 -0500

    Its broke and I don't know why

--- Data_Visualization/vis_ins.m
+++ Data_Visualization/vis_ins.m
@@ -1,5 +1,7 @@
 %% visualize gps ins
 % Shows gps inertial solution data
-date = "2014-06-25-16-22-15";
+if ~exist('date','var')
+    date = "2014-06-25-16-22-15";
+end
 [ins,time] = get_ins(date);
 visualize_state(ins,"gps_ins");
--- stateToOdometry.m
+++ stateToOdometry.m
@@ -7,11 +7,19 @@ function [odom] = stateToOdometry(state)
 %
 %   state ordering is in eular xyz
 %   [tx ty tz rx ry rz]'
-
+size(state)
 assert(size(state,1)==6,"State should have 6 rows");
 assert(size(state,2)>=2,"State mus have atleast 2 entries");
 num_states = size(state,2);
 
 odom = state(:,2:num_states)-state(:,1:num_states-1);
+for i = 1:num_states-1
+    state(:,i)
+    odom(:,i)
+    state(:,i+1)
+    prediction_step(state(:,i),[],odom(:,i))
+    prediction_step(state(:,i),[],odom(:,i)) == state(:,i+1)
+    prediction_step(state(:,i),[],odom(:,i)) - state(:,i+1)
+    assert(all(prediction_step(state(:,i),[],odom(:,i)) == state(:,i+1)))
 end
 
